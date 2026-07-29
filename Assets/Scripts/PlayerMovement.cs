311bcf7eb1097915b63f16c13a94c9402b996d32
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-07-29 04:39:37 -0300

    Add Player stairs climbing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,13 +12,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private float jumpImpulse;
     [SerializeField] private BoxCollider floorDetection;
+    [SerializeField] private Transform stepRaycastPointTransform;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float maxHeightToStep;
+    [SerializeField] private float stepFixHeight;
+    [SerializeField] private MeshCollider meshCollider;
 
     private Rigidbody rb;
     private ForceMode moveForceMode = ForceMode.Acceleration;
     private ForceMode jumpForceMode = ForceMode.Impulse;
 
     private float cameraEuler = 0f;
+    private float stepCenterRaycastOffset = 0.05f;
+    private float stepSidesRaycastOffset;
+
+    //In euler degrees
+    private float stepCheckRaycastSpread = 30f;
 
     private Vector3 axisInput = Vector3.zero;
     private Vector2 mouseInput = Vector2.zero;
@@ -27,6 +36,8 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
+        stepSidesRaycastOffset = stepCenterRaycastOffset * 10f;
+
         rb = GetComponent<Rigidbody>();
 
         Cursor.lockState = CursorLockMode.Locked;
@@ -54,6 +65,10 @@ public class PlayerMovement : MonoBehaviour
         cameraTransform.localRotation = Quaternion.Euler(cameraEuler, 0f, 0f);
 
         isOnAir = !Physics.CheckBox(floorDetection.bounds.center, floorDetection.bounds.extents, floorDetection.transform.rotation, groundLayer, QueryTriggerInteraction.Ignore);
+
+        FixStep();
+
+        Debug.Log(isOnAir);
 
         if (Input.GetButton("Jump") && !isOnAir)
         {
@@ -84,5 +99,78 @@ public class PlayerMovement : MonoBehaviour
             isOnAir = true;
             isJump = false;
         }
+    }
+
+    private void FixStep()
+    {
+        float distance = meshCollider.bounds.extents.x + stepCenterRaycastOffset;
+
+        Vector3[] raycasts = new Vector3[3]
+        {
+        stepRaycastPointTransform.position + transform.TransformDirection(axisInput) * distance,
+        stepRaycastPointTransform.position + (Quaternion.Euler(0f, stepCheckRaycastSpread, 0f) * transform.TransformDirection(axisInput) * (meshCollider.bounds.extents.x + stepSidesRaycastOffset)),
+        stepRaycastPointTransform.position + (Quaternion.Euler(0f, -stepCheckRaycastSpread, 0f) * transform.TransformDirection(axisInput) * (meshCollider.bounds.extents.x + stepSidesRaycastOffset))
+        };
+
+        foreach (Vector3 raycast in raycasts)
+        {
+            if (raycast != raycasts[0])
+            {
+                distance = meshCollider.bounds.extents.x + stepSidesRaycastOffset;
+            }
+
+            RaycastHit[] isCollidingStepRaycasts = Physics.RaycastAll(stepRaycastPointTransform.position, Vector3.Normalize(raycast - stepRaycastPointTransform.position), distance, groundLayer, QueryTriggerInteraction.Ignore);
+
+            bool isCollidingStep = isCollidingStepRaycasts.Length > 0;
+
+            if (isCollidingStep)
+            {
+                bool isStepHeightValid = false;
+
+                foreach (RaycastHit stepHit in isCollidingStepRaycasts)
+                {
+                    int stepId = stepHit.collider.GetInstanceID();
+
+                    RaycastHit[] isStepHeightValidRaycasts = Physics.RaycastAll(raycast + (Vector3.up * maxHeightToStep), Vector3.down, maxHeightToStep, groundLayer, QueryTriggerInteraction.Ignore);
+
+                    foreach (RaycastHit heightCheckHit in isStepHeightValidRaycasts)
+                    {
+                        if (heightCheckHit.collider.GetInstanceID() == stepId)
+                        {
+                            isStepHeightValid = true;
+                        }
+                    }
+
+                    if (!isStepHeightValid)
+                    {
+                        break;
+                    }
+                }
+
+                if (isCollidingStep && isStepHeightValid)
+                {
+                    transform.Translate(new Vector3(0f, stepFixHeight, 0f));
+                    break;
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 middleLine = stepRaycastPointTransform.position + transform.TransformDirection(axisInput) * (meshCollider.bounds.extents.x + stepCenterRaycastOffset);
+
+        Gizmos.DrawLine(stepRaycastPointTransform.position, middleLine);
+        Gizmos.DrawLine(middleLine, middleLine + Vector3.up * (maxHeightToStep));
+
+        float angle = 15f;
+
+        Vector3 leftLine = stepRaycastPointTransform.position + (Quaternion.Euler(0f, angle, 0f) * transform.TransformDirection(axisInput) * (meshCollider.bounds.extents.x + stepSidesRaycastOffset));
+
+        Gizmos.DrawLine(stepRaycastPointTransform.position, leftLine);
+
+        Vector3 rightLine = stepRaycastPointTransform.position + (Quaternion.Euler(0f, -angle, 0f) * transform.TransformDirection(axisInput) * (meshCollider.bounds.extents.x + stepSidesRaycastOffset));
+
+        Gizmos.DrawLine(stepRaycastPointTransform.position, rightLine);
     }
 }
