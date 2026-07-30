f95b490e054d52e7a92275f2732f2a92c29c88b8
AgustinLaure <agustin.laure@estudiante.imagecampus.edu.ar>
2026-07-30 07:19:21 -0300

    Add MeleeEnemy

---
+++ Assets/Scripts/MeleeEnemy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+
+public class MeleeEnemy : Enemy
+{
+    [SerializeField] private float damage;
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private SphereCollider attackRangeArea;
+
+    private Coroutine attackCoroutine;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        bool isAtRange = Physics.CheckSphere(
+                attackRangeArea.transform.TransformPoint(attackRangeArea.center),
+                attackRangeArea.radius,
+                targetLayerMask,
+                QueryTriggerInteraction.Ignore);
+
+        if (isAtRange)
+        {
+            if (attackCoroutine == null)
+            {
+                attackCoroutine = StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        Collider[] colliders = Physics.OverlapSphere(attackRangeArea.transform.TransformPoint(attackRangeArea.center),
+               attackRangeArea.radius,
+               targetLayerMask,
+               QueryTriggerInteraction.Ignore);
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.CompareTag(playerTag) || collider.CompareTag(enemyTag))
+            {
+                collider.GetComponent<HealthPoints>().TakeDamage(damage);
+            }
+        }
+
+        yield return new WaitForSeconds(attackCooldown);
+
+        attackCoroutine = null;
+    }
+}
